a3f65410e2bcc28f7ca2adc8e160f1d4e9725f13
teixeiraklaudio <claudio220503@gmail.com>
2026-04-21 15:59:52 -0300

    css e funções aos botões

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,32 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Calculator.css"
 import Container from '@mui/material/Container';
+import { Box } from '@mui/material';
 
 export default function Calculator() {
+    const [num,setNum] = useState(0); 
+
+    function inputNum(e) {
+      var input=e.target.value;
+      setNum(num + input);
+    }
   return (
-    <Container>
-    <div className='wrapper'>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>0</button>
-      <button>,</button>
-      <button>AC</button>
-      <button>+/-</button>
-      <button>%</button>
-      <button>/</button>
-      <button>X-</button>
-      <button>-</button>
-      <button>+-</button>
-      <button>=</button>
-      
+    <div>
+      <Box m={5} />
+      <Container maxWidth="xs">
+        <Box m={10} />
+        <div className='wrapper'>
+          <h1 className="result">{num}</h1>
+          <button className='lihtgray'>C</button>
+          <button className='lihtgray'>AC</button>
+          <button className='lihtgray'>%</button>
+          <button className='orange'>/</button>
+          <button className='gray' onClick={inputNum} value={7}>7</button>
+          <button className='gray'onClick={inputNum} value={8}>8</button>
+          <button className='gray'onClick={inputNum} value={9}>9</button>
+          <button className='orange'>X</button>
+          <button className='gray'onClick={inputNum} value={4}>4</button>
+          <button className='gray'onClick={inputNum} value={5}>5</button>
+          <button className='gray'onClick={inputNum} value={6}>6</button>
+          <button className='orange'>-</button>
+          <button className='gray'onClick={inputNum} value={1}>1</button>
+          <button className='gray'onClick={inputNum} value={2}>2</button>
+          <button className='gray'onClick={inputNum} value={3}>3</button>
+          <button className='orange'>+</button>
+          <button className='gray'>+/-</button>
+          <button className='gray'onClick={inputNum} value={0}>0</button>
+          <button className='gray'>,</button>
+          <button className='orange'>=</button>      
+        </div>
+      </Container>
     </div>
-    </Container>
-  )
+  );
 }
